42db206623a511c6e236853167cb6c47f2154e60
lirondadon <lirond101@icloud.com>
2022-09-18 22:37:51 +0300

    exercise 5

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+echo 'Host server2
+    HostName 192.168.60.11
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+    IdentityFile /vagrant/.vagrant/machines/server2/virtualbox/private_key' >> /home/vagrant/.ssh/config
+
+chown vagrant /home/vagrant/.ssh/config
+chgrp vagrant /home/vagrant/.ssh/config
+chmod 600 /home/vagrant/.ssh/config
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+echo 'Host *
+    PasswordAuthentication no' >> /home/vagrant/.ssh/config
+chown vagrant /home/vagrant/.ssh/config
+chgrp vagrant /home/vagrant/.ssh/config
+chmod 600 /home/vagrant/.ssh/config
